102c4a9dcbc7b1440147b88cbb0e6b76c24f3939
Sam Davies <sam@visualputty.co.uk>
2011-03-08 21:47:11 +0000

    JS doesn't work yet...

---
+++ static/js/passage_complete.js
@@ -0,0 +1,28 @@
+$(function() {
+    $( "#id_ship_name" ).autocomplete({
+        source: function( request, response ) {
+            $.ajax({
+                url: "/passages/ajax_lookup",
+                dataType: "json",
+                success: function( data ) {
+                    response( $.map( data.passages, function( item ) {
+                        return {
+                            label: item.ship_name,
+                            value: item.ship_name
+                        };
+                    }));
+                }
+            });
+        },
+        minLength: 2,
+        select: function( event, ui ) {
+            
+        },
+        open: function( ) {
+            
+        },
+        close: function( ) {
+            
+        }
+    });
+});
