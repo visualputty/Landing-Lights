2b72f96df83286842aaf5d280c9927dc9755092c
Sam Davies <sam.jc.davies@gmail.com>
2011-03-13 14:29:00 +0000

    Added logos and fixed some admin organisation.

--- passages/admin.py
+++ passages/admin.py
@@ -2,9 +2,23 @@ from passages.models import Passage, VesselDetail
 from django.contrib import admin
 
 class PassageAdmin(admin.ModelAdmin):
-    list_display = ('eta_breaksea', 'ship_name', 'date_added')
-    list_filter = ['eta_breaksea']
+    list_display = ('eta_breaksea', 'ship_name', 'dock_pilot', 'email_address',  'date_added')
     search_fields = ['ship_name']
+    fieldsets = (
+        (None, {
+            'fields': ('ship_name', 'eta_breaksea', 'etd_sharpness', 'email_address', 'dock_pilot')
+        }),
+        ('Further info', {
+            'classes': ('collapse',),
+            'fields': ('speed', 'bowthrust', 'radar', 'fwd_draft', 'aft_draft', 'other', 'no_persons',
+                       'next_port')
+        }),
+        ('Vessel info', {
+            'classes': ('collapse',),
+            'fields': ('imo', 'dwt', 'max_loa', 'max_beam', 'height', 'rudder')
+        }),
+    )
+    ordering = ('-eta_breaksea','ship_name')
 
 class VesselDetailAdmin(admin.ModelAdmin):
     list_display = ('ship_name', 'imo')
--- passages/dbindexes.py
+++ passages/dbindexes.py
@@ -1,3 +1,7 @@
-from models import VesselDetail
+from models import VesselDetail, Passage
 from dbindexer.api import register_index
 register_index(VesselDetail, {'ship_name': 'istartswith'})
+register_index(Passage, {'ship_name': 'icontains'})
+#register_index(Passage, {'date_added': 'day'})
+#register_index(Passage, {'date_added': 'month'})
+#register_index(Passage, {'date_added': 'year'})
